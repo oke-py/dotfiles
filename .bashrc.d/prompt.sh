c8d76a284133ecee88c00c395c3cc313c15c78ba
Naoki Oketani <okepy.naoki@gmail.com>
2015-11-22 00:48:44 +0900

    Fix PS1: escape $

--- .bashrc.d/prompt.sh
+++ .bashrc.d/prompt.sh
@@ -4,8 +4,8 @@ if type __git_ps1 > /dev/null 2>&1; then
     GIT_PS1_SHOWUNTRACKEDFILES=true
     GIT_PS1_SHOWUPSTREAM=auto
 
-    export PS1='\[\033[32m\]\u@\h\[\033[00m\]:\[\033[34m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\n$ '
+    export PS1='\[\033[32m\]\u@\h\[\033[00m\]:\[\033[34m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\n\$ '
 
 else
-    export PS1='\[\033[32m\]\u@\h\[\033[00m\]:\[\033[34m\]\w\[\033[00m\]\n$ '
+    export PS1='\[\033[32m\]\u@\h\[\033[00m\]:\[\033[34m\]\w\[\033[00m\]\n\$ '
 fi
