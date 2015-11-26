136fcc1a7171ee5db141526b82e7c2ff037a6777
Naoki Oketani <okepy.naoki@gmail.com>
2015-11-26 19:36:18 +0900

    Add .bash_profile

--- symlink.sh
+++ symlink.sh
@@ -4,6 +4,7 @@ BASEDIR=~/src/github.com/oke-py/dotfiles
 
 ln -sf $BASEDIR/.gitconfig ~/.gitconfig
 ln -sf $BASEDIR/.bashrc ~/.bashrc
+ln -sf $BASEDIR/.bash_profile ~/.bash_profile
 
 if ! [ -d ~/.bashrc.d ]; then
     ln -s $BASEDIR/.bashrc.d ~/.bashrc.d
